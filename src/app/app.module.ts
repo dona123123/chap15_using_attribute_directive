dc8b0a6ad54a34f65c98293e26b2f5ece85cb824
dongyul an <dongyul.an.123@gmail.com>
2020-12-28 15:15:31 -0500

    comit  --2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-// 12/28/20202 p342 
+// 12/28/20202 p350 
 
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { ProductComponent } from "./component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { PaAttrDirective } from "./attr.directive";
 
 @NgModule({
     imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-    declarations: [ProductComponent],
+    declarations: [ProductComponent, PaAttrDirective],
     bootstrap: [ProductComponent]
 })
 export class AppModule {}
---
+++ src/app/attr.directive.ts
@@ -0,0 +1,15 @@
+import { Attribute, Directive, ElementRef, Input } from "@angular/core";
+@Directive({
+    selector: "[pa-attr]",
+})
+export class PaAttrDirective {
+    constructor( private element: ElementRef){}
+
+    @Input("pa-attr")
+    bgClass: string;
+
+    ngOnInit(){
+        this.element.nativeElement.classList.add( this.bgClass || "bg-success", "text=-white");
+    }
+
+}
